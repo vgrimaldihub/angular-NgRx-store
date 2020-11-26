3ea7ddf864889131e586cfe55b2f36a7811db76c
vgrimaldihub <v.grimaldi1993@gmail.com>
2020-11-26 16:35:14 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+ 
 import { AppComponent } from './app.component';
-
+ 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+ 
 @NgModule({
-  imports: [BrowserModule],
   declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
