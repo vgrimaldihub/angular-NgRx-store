9f9aae4c9d397af97a8b2ed87c99bbfdd11b3f7e
EUROPE\vigrimaldi <vittorio@grimaldi@altran.it>
2020-11-26 17:06:02 +0100

    +commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
  
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.components';
  
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
   providers: [],
   bootstrap: [AppComponent],
